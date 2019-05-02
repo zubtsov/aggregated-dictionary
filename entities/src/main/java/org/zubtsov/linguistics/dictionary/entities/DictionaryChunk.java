cf03a9ad5403d42cf451ba94a65f077bafae21f8
Zubtsov Ruslan <18098732+zubtsov@users.noreply.github.com>
2019-05-02 17:22:57 +0300

    temporary commit

---
+++ entities/src/main/java/org/zubtsov/linguistics/dictionary/entities/DictionaryChunk.java
@@ -0,0 +1,7 @@
+package org.zubtsov.linguistics.dictionary.entities;
+
+import java.util.List;
+
+public class DictionaryChunk {
+    private List<Существительное> существительные;
+}
--- odict-parser/src/main/java/org/zubtsov/linguistics/dictionary/CsvLineObjectMapper.java
+++ odict-parser/src/main/java/org/zubtsov/linguistics/dictionary/CsvLineObjectMapper.java
@@ -1,7 +1,87 @@
 package org.zubtsov.linguistics.dictionary;
 
-public class CsvLineObjectMapper {
-    public void map(String[] line) {
+import lombok.extern.slf4j.Slf4j;
+import org.zubtsov.linguistics.dictionary.entities.DictionaryChunk;
 
+@Slf4j
+public class CsvLineObjectMapper {
+    //todo: implement
+    public DictionaryChunk map(String[] line) {
+        String modifier = line[1];
+        switch (modifier) {
+            case "с":
+
+                break;
+            case "мо":
+
+                break;
+            case "св-нсв":
+
+                break;
+            case "числ.":
+
+                break;
+            case "жо":
+
+                break;
+            case "мо-жо":
+
+                break;
+            case "мс-п":
+
+                break;
+            case "числ.-п":
+
+                break;
+            case "св":
+
+                break;
+            case "межд.":
+
+                break;
+            case "предл.":
+
+                break;
+            case "ж":
+
+                break;
+            case "вводн.":
+
+                break;
+            case "союз":
+
+                break;
+            case "част.":
+
+                break;
+            case "сравн.":
+
+                break;
+            case "м":
+
+                break;
+            case "предик.":
+
+                break;
+            case "н":
+
+                break;
+            case "мн.":
+
+                break;
+            case "со":
+
+                break;
+            case "нсв":
+
+                break;
+            case "п":
+
+                break;
+            default:
+                log.error("Unknown modifier: {}", modifier);
+                break;
+        }
+        return null;
     }
 }
